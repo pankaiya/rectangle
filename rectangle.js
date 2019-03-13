50807524977b85022a763328a718f45fd9d7f4c0
wangding <408542507@qq.com>
2019-03-13 21:37:03 +0800

    0.2代码静态检查

---
+++ rectangle.js
@@ -0,0 +1,17 @@
+$(function(){
+  var $length = $('#length'),
+      $width = $('#width'),
+      $btnCal = $('#btnCal'),
+      $perimeter=$('#perimeter'),
+      $area=$('#area');
+  $btnCal.click(function(){
+    var w = Number($length.val()),
+        h = Number($width.val());
+
+    var p = 2*(w+h),
+        a = w * h;
+    $perimeter.val(p);
+    $area.val(a);
+
+  });
+});
